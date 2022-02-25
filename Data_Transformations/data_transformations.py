1419f89de76eadc122ca8bc1cab4d885fdc5dbf8
Harry Edwards <h.edwards.2@outlook.com>
2022-02-25 16:03:17 +0000

    Create .py files and structure for data processing code.

--- Data_Transformations/data_transformations.py
+++ Data_Transformations/data_transformations.py
@@ -1,8 +1,5 @@
+# The purpose of this code is to perform all necessary generic data pre-processing of the raw data.
+# The output of this code should serve as the input to the predictive models.
+
 import os
 import pandas as pd
-
-data_path = os.path.dirname(os.getcwd()) + "\Data\Raw_Data\cleaned_merged_seasons.csv"
-
-df = pd.read_csv(data_path).iloc[:, 1:]
-
-print('End')
